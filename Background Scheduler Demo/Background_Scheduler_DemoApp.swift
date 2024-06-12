464cff7b05e953e5310f5ec555f7f572b0d20854
David Bureš <buresdv@gmail.com>
2024-06-12 13:06:54 +0200

    Initial Commit

---
+++ Background Scheduler Demo/Background_Scheduler_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Background_Scheduler_DemoApp.swift
+//  Background Scheduler Demo
+//
+//  Created by David Bureš on 12.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct Background_Scheduler_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
